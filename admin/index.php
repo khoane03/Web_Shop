0f303e75ed416aa39a887200932a86522c04d7f9
khoane03 <khoatk4444@gmail.com>
2023-11-14 01:33:22 +0700

    upcode

---
+++ admin/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ADMIN</title>
+    <link rel="stylesheet" href="../css/style.css">
+</head>
+<body>
+    <div class="wrapper">
+        <div class="header">
+           <div class="header-logo">
+
+           </div>
+        </div>
+        <div class="menu">
+
+        </div>
+        <div class="container">
+
+        </div>
+        <div class="footer">
+
+        </div>
+    </div>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,210 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TECHONE</title>
+    <link rel="icon" href="./images/logo.png">
+    <link rel="stylesheet" href="./css/style.css">±
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+
+<body>
+
+    <div class="wrapper">
+        <div class="header">
+            <ul class="header-left">
+                <li class="header-logo hiden_list">
+                    <a href="index.php"><img src="./images/logo.png" alt="logo"></a>
+                </li>
+                <li class="header-search hiden_list">
+                    <i class="fa-solid fa-magnifying-glass" style="color: #040404;"></i>
+                    <input type="text" class="search__input" name="" id="" placeholder="Bạn cần tìm gì ?">
+                </li>
+            </ul>
+            <ul class="header-righ">
+                <li class="header-support hiden_list text__separate">
+                    <i class="fa-solid fa-phone sp_icon" style="color: #444444;"></i>
+                    <a href="" class="sp__text ">hỏi đáp</a>
+                </li>
+                <li class="header-cart hiden_list text__separate">
+                    <i class="fa-solid fa-cart-shopping cart_icon" style="color: #444444;"></i>
+                    <a href="" class="cart__text ">giỏ hàng</a>
+                </li>
+                <li class="header-login hiden_list">
+                    <img src="./images/icon_login.png" alt="images login">
+                    <a href="" class="login__text ">Đăng nhập</a>
+                </li>
+            </ul>
+        </div>
+        <div class="banner">
+            <div class="slide fade">
+                <img src="./images/banner.png" alt="Slide 1">
+
+            </div>
+            <div class="slide fade">
+                <img src="./images/banner1.png" alt="Slide 2">
+            </div>
+            <div class="slide fade">
+                <img src="./images/banner2.png" alt="Slide 3">
+            </div>
+        </div>
+        <div class="menu">
+            <a href="index.php" class="menu__item "> <i class="fa-solid fa-house" style="color: #444444;"></i> Trang chủ</a>
+            <a href="" class="menu__item "><i class="fa-solid fa-mobile style=" color: #444444;></i> Điện thoại</a>
+            <a href="" class="menu__item "><i class="fa-solid fa-laptop" style="color: #444444;"></i> Laptop</a>
+            <a href="" class="menu__item "><i class="fa-solid fa-tablet" style="color: #444444;"></i> Tablet</a>
+            <a href="" class="menu__item "><i class="fa-regular fa-clock" style="color: #444444;"></i> Watch</a>
+        </div>
+        <div class="container">
+            <div class="products">
+                <a href="" class="product-item hiden_list">
+                    <img class="item__image" src="./images/14-black.jpeg" alt="">
+                    <div class="item__name">
+                        <span>IPhone 14 ProMax </span>
+                    </div>
+                    <div class="item__price">
+                        <span class="item__cost">35.000.000đ</span>
+                        <span class="item__sale">- 30%</span>
+                    </div>
+                </a>
+                <a href="" class="product-item hiden_list">
+                    <img class="item__image" src="./images/14-black.jpeg" alt="">
+                    <div class="item__name">
+                        <span>IPhone 14 ProMax </span>
+                    </div>
+                    <div class="item__price">
+                        <span class="item__cost">35.000.000đ</span>
+                        <span class="item__sale">- 30%</span>
+                    </div>
+                </a>
+                <a href="" class="product-item hiden_list">
+                    <img class="item__image" src="./images/14-black.jpeg" alt="">
+                    <div class="item__name">
+                        <span>IPhone 14 ProMax </span>
+                    </div>
+                    <div class="item__price">
+                        <span class="item__cost">35.000.000đ</span>
+                        <span class="item__sale">- 30%</span>
+                    </div>
+                </a>
+                <a href="" class="product-item hiden_list">
+                    <img class="item__image" src="./images/14-black.jpeg" alt="">
+                    <div class="item__name">
+                        <span>IPhone 14 ProMax </span>
+                    </div>
+                    <div class="item__price">
+                        <span class="item__cost">35.000.000đ</span>
+                        <span class="item__sale">- 30%</span>
+                    </div>
+                </a>
+                <a href="" class="product-item hiden_list">
+                    <img class="item__image" src="./images/14-black.jpeg" alt="">
+                    <div class="item__name">
+                        <span>IPhone 14 ProMax </span>
+                    </div>
+                    <div class="item__price">
+                        <span class="item__cost">35.000.000đ</span>
+                        <span class="item__sale">- 30%</span>
+                    </div>
+                </a>
+                <a href="" class="product-item hiden_list">
+                    <img class="item__image" src="./images/14-black.jpeg" alt="">
+                    <div class="item__name">
+                        <span>IPhone 14 ProMax </span>
+                    </div>
+                    <div class="item__price">
+                        <span class="item__cost">35.000.000đ</span>
+                        <span class="item__sale">- 30%</span>
+                    </div>
+                </a>
+                <a href="" class="product-item hiden_list">
+                    <img class="item__image" src="./images/14-black.jpeg" alt="">
+                    <div class="item__name">
+                        <span>IPhone 14 ProMax </span>
+                    </div>
+                    <div class="item__price">
+                        <span class="item__cost">35.000.000đ</span>
+                        <span class="item__sale">- 30%</span>
+                    </div>
+                </a>
+                <a href="" class="product-item hiden_list">
+                    <img class="item__image" src="./images/14-black.jpeg" alt="">
+                    <div class="item__name">
+                        <span>IPhone 14 ProMax </span>
+                    </div>
+                    <div class="item__price">
+                        <span class="item__cost">35.000.000đ</span>
+                        <span class="item__sale">- 30%</span>
+                    </div>
+                </a>
+                <a href="" class="product-item hiden_list">
+                    <img class="item__image" src="./images/14-black.jpeg" alt="">
+                    <div class="item__name">
+                        <span>IPhone 14 ProMax </span>
+                    </div>
+                    <div class="item__price">
+                        <span class="item__cost">35.000.000đ</span>
+                        <span class="item__sale">- 30%</span>
+                    </div>
+                </a>
+                <a href="" class="product-item hiden_list">
+                    <img class="item__image" src="./images/14-black.jpeg" alt="">
+                    <div class="item__name">
+                        <span>IPhone 14 ProMax </span>
+                    </div>
+                    <div class="item__price">
+                        <span class="item__cost">35.000.000đ</span>
+                        <span class="item__sale">- 30%</span>
+                    </div>
+                </a>
+            </div>
+        </div>
+        <div class="footer">
+                <div class="footer-top">
+
+                </div>
+                <div class="footer--copyringht">
+                    <p>@2023</p>
+                </div>
+            </div>
+        </div>
+        <script>
+            var slideIndex = 1; // Bắt đầu từ slide đầu tiên
+
+            function showSlides(n) {
+                let slides = document.getElementsByClassName("slide");
+
+                if (n > slides.length) {
+                    slideIndex = 1;
+                }
+
+                if (n < 1) {
+                    slideIndex = slides.length;
+                }
+
+                for (let i = 0; i < slides.length; i++) {
+                    slides[i].style.display = "none";
+                }
+
+                slides[slideIndex - 1].style.display = "block";
+            }
+
+            function plusSlides(n) {
+                showSlides(slideIndex += n);
+            }
+
+            showSlides(slideIndex);
+
+            // Tự động chuyển slide sau một khoảng thời gian
+            function autoSlide() {
+                plusSlides(1);
+                setTimeout(autoSlide, 2000); // Đổi slide mỗi 2 giây
+            }
+
+            autoSlide();
+        </script>
+</body>
+
+</html>
